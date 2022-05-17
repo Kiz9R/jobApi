19c65883b601cbee1bcb816d1936f81a96fc5df0
kiz9r <ritankarxkiz9r@gmail.com>
2022-05-17 21:36:58 +0530

    initial commit

--- models/User.js
+++ models/User.js
@@ -33,9 +33,19 @@ userSchema.pre("save", async function (next) {
 });
 
 userSchema.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id, name: this.name }, `jwtSecret`, {
-    expiresIn: `30d`,
-  });
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+};
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+
+  return isMatch;
 };
 
 module.exports = mongoose.model(`User`, userSchema);
