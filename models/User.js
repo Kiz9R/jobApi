e04836317a54fb93353380c3e9afb30ec591cb1d
kiz9r <ritankarxkiz9r@gmail.com>
2022-05-17 21:11:26 +0530

    initial commit

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require(`bcryptjs`);
-
+const jwt = require(`jsonwebtoken`);
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -31,5 +31,11 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+
+userSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, `jwtSecret`, {
+    expiresIn: `30d`,
+  });
+};
 
 module.exports = mongoose.model(`User`, userSchema);
