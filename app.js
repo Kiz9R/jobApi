1f27a24971d7765d2343aec789262e4d0badcd62
kiz9r <ritankarxkiz9r@gmail.com>
2022-05-20 20:54:20 +0530

    initial commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 // connectDB
 
 const connectDB = require("./db/connect");
+const authenticateUser = require(`./middleware/authentication`);
 
 // routers
 
@@ -21,7 +22,7 @@ app.use(express.json());
 
 // routes
 app.use(`/api/v1/auth`, authRouter);
-app.use(`/api/v1/jobs`, jobRouter);
+app.use(`/api/v1/jobs`, authenticateUser, jobRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
